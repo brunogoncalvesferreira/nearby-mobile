85ac4424bac2a7ca6c2f10e97f1f4d5fa10fcd60
brunogoncalvesferreira <brunogoncalvesferreira@outlook.com>
2025-05-15 21:20:18 -0300

    mobile nearby

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://192.168.100.88:3333",
+  //baseURL: "http://192.168.100.88:3333",
+  baseURL: 'http://10.0.2.2:3333',
   timeout: 700
 })
